e37bb6517142e4576cdd43ddbf935735366550bc
alinarozental <alina.rosenthal33@icloud.com>
2025-10-22 20:40:43 +0200

    header + Mobile menu адаптация готова

--- js/modal.js
+++ js/modal.js
@@ -16,3 +16,18 @@
     refs.modal.classList.toggle("is-open");
   }
 })();
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector(".mobile-menu"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-open");
+  }
+})();
